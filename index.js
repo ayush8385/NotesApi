992a166d475dc4f1a6a3694d9999c078820d9da8
Ayush Mishra <63926489+ayush8385@users.noreply.github.com>
2023-05-15 05:48:43 +0530

    magic

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log("Server Started on port no"+PORT)
 })
-mongoose.connect('mongodb+srv://ayush8385:Ayush8385@notes.tk6z9sp.mongodb.net/').then(()=>console.log("connnected to DBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")).catch(()=>{
+mongoose.connect('mongodb+srv://ayush8385:Ayush8385@notes.tk6z9sp.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log("connnected to DBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")).catch(()=>{
     console.log("Error in Connecting to DB")
 })
 
